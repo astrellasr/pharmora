42d2d8cb5fa03d6c1fb528451b49aa78248dc240
astrellasr <astrellaramadhante@gmail.com>
2026-07-18 17:37:22 +0700

    update product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,9 +15,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
+        $categories = Category::pluck('id', 'code');
+        $suppliers = Supplier::pluck('id', 'code');
+
         $products = [
             [
                 'sku' => 'PRD-000001',
+                'barcode' => '8991002000001',
                 'name' => 'Paracetamol 500 mg',
                 'category' => 'CAT-001',
                 'supplier' => 'SUP-001',
@@ -26,6 +30,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000002',
+                'barcode' => '8991002000002',
                 'name' => 'Amoxicillin 500 mg',
                 'category' => 'CAT-001',
                 'supplier' => 'SUP-002',
@@ -34,6 +39,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000003',
+                'barcode' => '8991002000003',
                 'name' => 'Cetirizine 10 mg',
                 'category' => 'CAT-002',
                 'supplier' => 'SUP-003',
@@ -42,6 +48,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000004',
+                'barcode' => '8991002000004',
                 'name' => 'Omeprazole 20 mg',
                 'category' => 'CAT-001',
                 'supplier' => 'SUP-004',
@@ -50,6 +57,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000005',
+                'barcode' => '8991002000005',
                 'name' => 'Vitamin C 1000 mg',
                 'category' => 'CAT-003',
                 'supplier' => 'SUP-001',
@@ -58,6 +66,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000006',
+                'barcode' => '8991002000006',
                 'name' => 'Vitamin D3',
                 'category' => 'CAT-003',
                 'supplier' => 'SUP-005',
@@ -66,6 +75,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000007',
+                'barcode' => '8991002000007',
                 'name' => 'Digital Thermometer',
                 'category' => 'CAT-004',
                 'supplier' => 'SUP-006',
@@ -74,6 +84,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000008',
+                'barcode' => '8991002000008',
                 'name' => 'Blood Pressure Monitor',
                 'category' => 'CAT-004',
                 'supplier' => 'SUP-006',
@@ -82,6 +93,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000009',
+                'barcode' => '8991002000009',
                 'name' => 'Hand Sanitizer 500 mL',
                 'category' => 'CAT-005',
                 'supplier' => 'SUP-005',
@@ -90,6 +102,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000010',
+                'barcode' => '8991002000010',
                 'name' => 'Surgical Mask',
                 'category' => 'CAT-005',
                 'supplier' => 'SUP-002',
@@ -98,6 +111,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000011',
+                'barcode' => '8991002000011',
                 'name' => 'Baby Lotion',
                 'category' => 'CAT-006',
                 'supplier' => 'SUP-003',
@@ -106,6 +120,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'sku' => 'PRD-000012',
+                'barcode' => '8991002000012',
                 'name' => 'Baby Diapers',
                 'category' => 'CAT-006',
                 'supplier' => 'SUP-001',
@@ -117,10 +132,10 @@ class ProductSeeder extends Seeder
         foreach ($products as $product) {
             Product::create([
                 'uuid' => (string) Str::uuid(),
-                'category_id' => Category::where('code', $product['category'])->firstOrFail()->id,
-                'supplier_id' => Supplier::where('code', $product['supplier'])->firstOrFail()->id,
+                'category_id' => $categories[$product['category']],
+                'supplier_id' => $suppliers[$product['supplier']],
                 'sku' => $product['sku'],
-                'barcode' => fake()->unique()->ean13(),
+                'barcode' => $product['barcode'],
                 'name' => $product['name'],
                 'description' => null,
                 'current_stock' => $product['stock'],
