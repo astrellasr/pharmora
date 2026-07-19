8104eb83651e52a3396935bfac8d539c8a69f6fd
astrellasr <astrellaramadhante@gmail.com>
2026-07-19 23:57:18 +0700

    update sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,9 +1,72 @@
-<aside>
+<aside class="w-64 shrink-0 border-r border-slate-200 bg-white">
 
-    {{-- Logo --}}
+    <div class="flex h-full flex-col">
 
-    {{-- Navigation --}}
+        {{-- Logo --}}
+        <div class="px-6 py-6">
+            <h1 class="text-xl font-bold">
+                Pharmora
+            </h1>
+        </div>
 
-    {{-- Footer Navigation --}}
+        {{-- Main Navigation --}}
+        <nav class="flex-1 px-4">
+            <ul class="space-y-2">
+
+                <li>
+                    <a href="#">
+                        Dashboard
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        Products
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        Categories
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        Suppliers
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        Inventory
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+        {{-- Footer Navigation --}}
+        <div class="border-t border-slate-200 p-4">
+
+            <ul class="space-y-2">
+
+                <li>
+                    <a href="#">
+                        Profile
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        Settings
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
 
 </aside>
