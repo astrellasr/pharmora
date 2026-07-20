5bb6feba9f1082b9e5fa4ad90a8a4200f7056d97
astrellasr <astrellaramadhante@gmail.com>
2026-07-20 23:52:35 +0700

    feat(ui): add reusable badge component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,19 @@
 <x-app-layout>
-    <div class="p-6">
-        Dashboard
+
+    <div class="p-6 space-y-4">
+
+        <x-ui.badge variant="success">
+            Active
+        </x-ui.badge>
+
+        <x-ui.badge variant="danger">
+            Critical
+        </x-ui.badge>
+
+        <x-ui.badge variant="warning">
+            Alerting
+        </x-ui.badge>
+
     </div>
+
 </x-app-layout>
